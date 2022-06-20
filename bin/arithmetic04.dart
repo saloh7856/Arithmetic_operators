025f7a7a28c5a2dd5a31362729867d4e1d2fabf2
saloh7856 <salohiddinberdiyorov2002@gmail.com>
2022-06-20 14:31:21 -0700

    dan

--- bin/arithmetic04.dart
+++ bin/arithmetic04.dart
@@ -2,4 +2,11 @@
 // Create function arguments a and b of type int.
 // Return the multiplication of a and b.
 
-void main() {}
+int multiple(int a,int  b){
+return a*b;
+  
+}
+void main(){
+    
+    print(multiple(7, 2));
+}
