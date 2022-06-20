8835f7defab7c90327598d599ab02e4154963d89
saloh7856 <salohiddinberdiyorov2002@gmail.com>
2022-06-20 14:25:26 -0700

    dan

--- bin/arithmetic03.dart
+++ bin/arithmetic03.dart
@@ -2,4 +2,11 @@
 // CCreate function arguments a and b of type int.
 // Returns the remainder of a and b.
 
-void main() {}
+int func(int a,int  b){
+return a%b;
+  
+}
+void main(){
+    
+    print(func(7, 2));
+}
