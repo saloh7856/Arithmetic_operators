52ba0b892720c2dc3e93571c28a2e03be47a063b
saloh7856 <salohiddinberdiyorov2002@gmail.com>
2022-06-20 20:27:33 -0700

    dan

--- bin/arithmetic07.dart
+++ bin/arithmetic07.dart
@@ -6,5 +6,14 @@
 // Find the ‘number’ third digit and assign to x3.
 // Create a variable called ‘answer’ and assign it the sum of the three digits.
 // return the sum of the digits.
-
-void main() {}
+int func(int number){
+    int x1,x2,x3, answer;
+    x1 = number~/100;
+    x2 = (number~/10)%10;
+    x3 = number%10;
+    answer = x1 + x2 + x3;
+    return answer;
+}
+void main(){
+    print(func(456));
+}
